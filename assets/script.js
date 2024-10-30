71328c0fca8ba57f6d75f4ee55f93e80029e6d71
Leonardo Bernardino <leonardobernardino12@gmail.com>
2024-10-30 17:54:55 -0300

    new updates

--- assets/script.js
+++ assets/script.js
@@ -10,7 +10,7 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
 
   if (html.classList.contains('light')){
-    img.setAttribute("src" , "./assets/./assets/avatar-light.png")
+    img.setAttribute("src", "assets/__MACOSX/assets/img-leo.png")
   } else {
     img.setAttribute("src", "assets/__MACOSX/assets/leozera.png")
     }
